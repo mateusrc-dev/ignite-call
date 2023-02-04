fb8cc2fb3052a612d8c2ec8bac7f005af633727b
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-04 00:59:52 -0300

    add react-hook-form in component ClainUsernameForm

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,12 +1,32 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  // vamos criar a estrutura dos dados que vamos receber do formulário
+  username: z.string(), // username é o nome do nosso input
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema> // inferindo a partir do nosso schema uma estrutura TypeScript
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       {/* Box vem da biblioteca como uma 'div', vamos mudar para 'form' */}
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
