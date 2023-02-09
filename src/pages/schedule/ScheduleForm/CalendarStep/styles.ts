9261cb8cf7829832632b276484addf5f38342a1e
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-09 15:47:26 -0300

    create component TimePicker

--- src/pages/schedule/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/ScheduleForm/CalendarStep/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, styled, Text } from '@ignite-ui/react'
 
 export const Container = styled(Box, {
   margin: '$6 auto 0',
@@ -7,6 +7,78 @@ export const Container = styled(Box, {
   maxWidth: '100%',
   position: 'relative',
 
-  width: 540,
+  variants: {
+    isTimePickerOpen: {
+      // vamos criar uma propriedade que pode ser passada pra esse componente
+      true: {
+        gridTemplateColumns: '1fr 280px', // cada coluna vai ter 280px
+
+        '@media(max-width: 900px)': {
+          gridTemplateColumns: '1fr', // para ficar apenas uma coluna
+        },
+      },
+      false: {
+        width: 540,
+        gridTemplateColumns: '1fr',
+      },
+    },
+  },
+})
+
+export const TimePicker = styled('div', {
+  borderLeft: '1px solid $gray600',
+  padding: '$6 $6 0', // o padding em baixo não funciona quando tem scroll - vamos usar marginBottom no último filho
+  overflowY: 'scroll',
+  position: 'absolute',
+  top: 0, // colocando 0 em top e bottom a altura desse componente vai ser definida de acordo com a altura do calendar
+  bottom: 0,
+  right: 0,
+  width: 280,
+})
+
+export const TimePickerHeader = styled(Text, {
+  fontWeight: '$medium',
+  span: {
+    color: '$gray200',
+  },
+})
+
+export const TimePickerList = styled('div', {
+  marginTop: '$3',
+  display: 'grid',
   gridTemplateColumns: '1fr',
+  gap: '$2',
+
+  '@media(max-width: 900px)': {
+    gridTemplateColumns: '2fr',
+  },
+})
+
+export const TimePickerItem = styled('button', {
+  border: 0,
+  backgroundColor: '$gray600',
+  padding: '$2 0',
+  cursor: 'pointer',
+  color: '$gray100',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  lineHeight: '$base',
+
+  '&:last-child': {
+    marginBottom: '$6',
+  },
+
+  '&:disabled': {
+    background: 'none',
+    cursor: 'default',
+    opacity: 0.4,
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500',
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100',
+  },
 })
