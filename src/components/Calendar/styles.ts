f7b19e51c5f280e2cb3d8e9f5111ef6b40b07eb1
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-09 14:09:56 -0300

    update component calendar

--- src/components/Calendar/styles.ts
+++ src/components/Calendar/styles.ts
@@ -1,13 +1,94 @@
 import { styled, Text } from '@ignite-ui/react'
 
-export const CalendarContainer = styled('div', {})
+export const CalendarContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$6',
+  padding: '$6',
+})
 
-export const CalendarHeader = styled('div', {})
+export const CalendarHeader = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+})
 
-export const CalendarTitle = styled(Text, {})
+export const CalendarTitle = styled(Text, {
+  fontWeight: '$medium',
+  span: { color: '$gray200' },
+})
 
-export const CalendarActions = styled('div', {})
+export const CalendarActions = styled('div', {
+  display: 'flex',
+  gap: '$2',
+  color: '$gray200',
 
-export const CalendarBody = styled('table', {})
+  button: {
+    all: 'unset', // tira os valores padrão das propriedades css
+    cursor: 'pointer',
+    lineHeight: 0, // tamanho do botão é definido com base no lineHeight
+    borderRadius: '$sm',
 
-export const CalendarDay = styled('button', {})
+    svg: {
+      width: '$5',
+      height: '$5',
+    },
+
+    '&:hover': {
+      color: '$gray100',
+    },
+
+    '&:focus': {
+      boxShadow: '0 0 0 2px $colors$gray100',
+    },
+  },
+})
+
+export const CalendarBody = styled('table', {
+  width: '100%', // por padrão o width da tabela não é 100%
+  fontFamily: '$default', // por padrão a font da tabela não é a mesma do documento
+  borderSpacing: '0.25rem',
+  tableLayout: 'fixed', // para calcular tamanho das colunas - fixed é para as células da tabela ter o mesmo tamanho
+
+  'thead th': {
+    color: '$gray200',
+    fontWeight: '$medium',
+    fontSize: '$sm',
+  },
+
+  'tbody:before': {
+    // para colocar um distanciamento entre thead e tbody
+    content: '.',
+    lineHeight: '0.75rem',
+    display: 'block',
+    color: '$gray800',
+  },
+
+  'tbody td': {
+    boxSizing: 'border-box',
+  },
+})
+
+export const CalendarDay = styled('button', {
+  all: 'unset',
+  width: '100%',
+  aspectRatio: '1 / 1', // para o botão ter a mesma largura e altura independente do tamanho da tela
+  background: '$gray600',
+  textAlign: 'center',
+  cursor: 'pointer',
+  borderRadius: '$sm',
+
+  '&:disabled': {
+    background: 'none',
+    cursor: 'default',
+    opacity: 0.4,
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500',
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100',
+  },
+})
--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -93,7 +93,7 @@ export default function TimeInterval() {
 
   const router = useRouter()
 
-  const weekDays = getWeekDays()
+  const weekDays = getWeekDays({ short: false })
 
   const { fields } = useFieldArray({
     control, // para saber qual formulário esse método está lidando
