f5d6f7a3d8301932b06192d7eb9e793ade2f9073
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-12 16:14:29 -0300

    add refresh token google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -16,6 +16,9 @@ export function buildNextAuthOptions(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
         authorization: {
           params: {
+            prompt: 'consent', // para que o refresh_token seja criado e com isso seja possível ficar mais tmepo logado
+            access_type: 'offline',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar', // aqui é para pedir a autorização ao usuário do google calendar e dados do usuário
           },
