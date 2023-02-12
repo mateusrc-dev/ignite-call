6d1890503b1106bfe305e0f3acfc3413ad89bb30
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-12 13:27:24 -0300

    create scheduling update

--- src/pages/schedule/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/ScheduleForm/CalendarStep/index.tsx
@@ -17,7 +17,11 @@ interface Availability {
   availableTimes: number[] // horários disponíveis
 }
 
-export function CalendarStep() {
+interface CalendarStepProps {
+  onSelectDateTime: (date: Date) => void
+}
+
+export function CalendarStep({ onSelectDateTime }: CalendarStepProps) {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
   // const [availability, setAvailability] = useState<Availability | null>(null)
   const router = useRouter()
@@ -49,6 +53,15 @@ export function CalendarStep() {
     },
   )
 
+  function handleSelectTime(hour: number) {
+    // função vai ser chamada quando o usuário selecionar o time
+    const dateWithTime = dayjs(selectedDate)
+      .set('hour', hour)
+      .startOf('hour')
+      .toDate() // colocando a hora selecionada na data selecionada no início exato da hora(startOf) - toDate é para colocar no formato de data do JS
+    onSelectDateTime(dateWithTime) // função de atualização do estado que está em ScheduleForm
+  }
+
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
@@ -63,6 +76,7 @@ export function CalendarStep() {
               return (
                 <TimePickerItem
                   key={hour}
+                  onClick={() => handleSelectTime(hour)}
                   disabled={!availability.availableTimes?.includes(hour)} // se não existir o 'hour' dentro de availableTimes é porque o horário não está disponível
                 >
                   {String(hour).padStart(2, '0')}:00h
